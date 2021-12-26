281ec592c5a7ac04c672bb090fc9644608a3f47e
Suchethkashyap <sucheth.kashyap2001@gmail.com>
2021-12-26 14:48:15 +0530

    Intializing project

---
+++ admin.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['login'])) {
+        header('LOCATION:welcom.php'); die();
+    }
+?>
+<html>
+    <head>
+    <?php include('templates/header.php'); ?>
+        <title>Admin Page</title>
+    </head>
+    <body>
+        This is admin page view able only by logged in users.
+    </body> 
+</html>
---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<head>
+	<title>welcome</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	.brand{
+	  	background: #cbb09c !important;
+	  }
+  	.brand-text{
+  		color: #cbb09c !important;
+  	}
+  	/* form{
+  		max-width: 460px;
+  		margin: 20px auto;
+  		padding: 20px;
+  	} */
+  </style>
+</head>
+<body class="grey lighten-4">
+	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="#" class="brand-logo brand-text">My Employee Manager</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="#" class="btn brand z-depth-0">Home</a></li>
+		
+		
+		
+		<li><a href="../logout.php" class="btn brand z-depth-0">Log out</a></li>
+      </ul>
+    </div>
+  </nav>
